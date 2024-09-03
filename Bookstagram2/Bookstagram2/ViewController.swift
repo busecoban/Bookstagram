a58b60e6000def19beaed4cbaae34aa925f21b06
Buse Çoban <73944611+busecoban@users.noreply.github.com>
2024-09-03 11:15:25 +0300

    initial project

---
+++ Bookstagram2/Bookstagram2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bookstagram2
+//
+//  Created by Buse Çoban on 3.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
